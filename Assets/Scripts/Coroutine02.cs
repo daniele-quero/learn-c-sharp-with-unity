ec6643d27d37bacb2db1d6a7b1416b6c9e70290f
danio24 <danio.quero@gmail.com>
2021-09-01 19:37:56 +0200

    coroutines

--- Assets/Scripts/Coroutine02.cs
+++ Assets/Scripts/Coroutine02.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Coroutine02 : MonoBehaviour
 {
     public bool goRoutine = true;
+
+    [SerializeField]
+    private MeshRenderer _meshRenderer;
+    private WaitForSeconds wait;
     // Start is called before the first frame update
     void Start()
     {
+        wait = new WaitForSeconds(2f);
         StartCoroutine(ChangeColor());
     }
 
@@ -21,11 +26,10 @@ public class Coroutine02 : MonoBehaviour
     {
         while (goRoutine)
         {
-
-            yield return new WaitForSeconds(2f);
+            yield return wait;
             Color randomColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             Debug.Log(randomColor);
-            GetComponent<MeshRenderer>().material.color = randomColor; 
+            _meshRenderer.material.color = randomColor; 
         }
         
     }
